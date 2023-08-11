c3eedc2287d62b98a119e8d4307dfda92a138a34
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2023-08-11 19:49:48 +0200

    jdhhdhd

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,11 +1,6 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-96a3#k(or!%2oi+76-=bofcs4)ees9rz@8muc2$f4uz&p+qqpe'
